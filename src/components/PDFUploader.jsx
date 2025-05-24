ee645729e7ee12db323ec425535ba30bd5edfa2a
masafun <ito@ii-stylelab.com>
2025-05-24 23:37:19 +0900

    Add files via upload

--- src/components/PDFUploader.jsx
+++ src/components/PDFUploader.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { supabase } from '../supabaseClient';
 
 export default function PDFUploader() {
   const [selectedFiles, setSelectedFiles] = useState([]);
